16d5e268593c8a37eb2fb092f861bcbc6e8a2f5e
manit <ermanit007@gmail.com>
2017-07-10 14:45:09 +0530

    Requestqueue

--- app/src/main/java/com/grademojo/volley2/AppController.java
+++ app/src/main/java/com/grademojo/volley2/AppController.java
@@ -50,3 +50,25 @@ public class AppController extends Application {
         return this.mImageLoader;
     }
 
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : req);
+        getRequestQueue().add(req);
+
+
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+
+
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+
+
+    }
+}
